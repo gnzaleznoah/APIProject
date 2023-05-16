49c43bad10de3d391867b32ceea8b2ac98c20c47
Noah <gnzalezmnoah@gmail.com>
2023-05-16 11:58:05 -0700

    refactoring

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const difficulty = [
     "intermediate",
     "expert"
 ];
+
 let exercise = [];
 let muscleGroup = [];
 let code = '';
 
+let workout1 = document.getElementById('workout1');
 let difficulty1 = document.getElementById('difficulty1');
 
 function init(){    
@@ -28,13 +30,11 @@ function init(){
     .catch((err) => console.log("Nope!!", err));
 
     //For Dropdown Menu
-    let workout1 = document.getElementById('workout1');
-
-    workout1.addEventListener('change',(e) => {
+     workout1.addEventListener('change',(e) => {
         code = e.target.value;
 
         const url = 'https://api.api-ninjas.com/v1/exercises?muscle=' + code;
-        
+        console.log(url);
         fetch( url, {
             method: "GET",
             headers: {
@@ -63,13 +63,8 @@ function init(){
     const container = document.querySelector('.work-out-list');
     container.innerHTML = "";
 
-    if(exercise.length === 0){
-        let displayMessage = document.createElement("P");
-        displayMessage.innerHTML = "Select a Muscle to Show Exercises";
-        container.appendChild(displayMessage);
-    }
-    else {        
-        exercise.forEach((item) =>{  
+        if(exercise.value === exercise.value){
+            exercise.forEach((item) =>{  
             let workoutPrinted = `<section class="set ${item.difficulty}">
                                 <p>Workout: ${item.name}</p>
                                 <p>Type: ${item.type}</p>
@@ -77,39 +72,41 @@ function init(){
                                 <p>Instructions: ${item.instructions}</p>
                                 </section>`
                                 ;
-    
-            let workList = document.querySelector('.work-out-list');
-            workList.innerHTML += workoutPrinted;
+                let workList = document.querySelector('.work-out-list');
+                workList.innerHTML += workoutPrinted;
             })
-
-    }
-
-
-
+        }
+        else {       
+            let displayMessage = document.createElement("P");
+            displayMessage.innerHTML = "Select a Muscle to Show Exercises";
+            container.appendChild(displayMessage);
+            console.log(exercise); 
+        }
         const diffLabel = document.querySelector('#difficultyLabel');
         diffLabel.classList.remove("hide");
-
-        const beg = document.querySelector('.beginner');
-        const int = document.querySelector('.intermediate');
-        const exp = document.querySelector('.expert ');
-
-        difficulty1.addEventListener('change', (e) => {
-            const intensity = e.target.value;
-            console.log(intensity);
-
-            if(intensity === "beginner"){
-                int.classList('.hide');
-                exp.classList('.hide')
-            }
-            else if(intensity === "intermediate"){
-                beg.classList('.hide');
-                exp.classList('.hide')
-            }
-            else if (intensity === 'expert'){
-                beg.classList('.hide');
-                int.classList('.hide')
-            }
+        console.log(exercise);
         });
+
+    const beg = document.querySelector('.beginner');
+    const int = document.querySelector('.intermediate');
+    const exp = document.querySelector('.expert ');
+    
+    difficulty1.addEventListener('change', (e) => {
+        const intensity = e.target.value;
+        console.log(intensity);
+
+        if(intensity.value === 'beginner'){
+            int.classList('.hide');
+            exp.classList('.hide')
+        }
+        else if(intensity.value === 'intermediate'){
+            beg.classList('.hide');
+            exp.classList('.hide')
+        }
+        else if (intensity.value === 'expert'){
+            beg.classList('.hide');
+            int.classList('.hide')
+        }
     });
 }
 
