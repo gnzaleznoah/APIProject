1448777ad1c8bb2a0eb8e1dc3e111f6c92af6bda
Noah Gonzalez <gnzalezmnoah@gmail.com>
2023-05-15 23:15:35 -0700

    update functions

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', init);
 
-function init(){
+const muscleGroup = [
+    "biceps",
+    "abdominals",
+    "hamstrings",
+    "quadriceps",
+    "glutes",
+    "chest"
+];
+const difficulty = [
+    "beginner",
+    "intermediate",
+    "expert"
+];
+let exercise = [];
 
-    let muscles = '';
-    const muscleGroup = [
-        "biceps",
-        "abdominals",
-        "hamstrings",
-        "quadriceps",
-        "glutes",
-        "chest"
-    ];
-    const difficulty = [
-        "Beginner",
-        "Intermediate",
-        "Expert"
-    ];
+function init(){
 
     let workout1 = document.getElementById('workout1');
     muscleGroup.forEach((item) =>{
         const option = document.createElement('OPTION');
-        option.value = item.codePointAt;
+        option.value = item;
         option.innerText = item;
         workout1.appendChild(option);
     });
@@ -34,14 +34,10 @@ function init(){
 
     workout1.addEventListener('change',(e) => {
         const code = e.target.value;
-        muscles = e.target.value;
-    });
 
-    var muscle = muscles;
-    const url = 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle;
-    
-
-    fetch( url, {
+        const url = 'https://api.api-ninjas.com/v1/exercises?muscle=' + code;
+        
+        fetch( url, {
         method: "GET",
         headers: {
             'X-Api-Key': 'IGWz0KSPZ9x79d4rDs6Phw==BPLz2nz7Uzilln0R'
@@ -57,34 +53,54 @@ function init(){
         .then((response) => response.json())
         .then((data) => {
             exercise = data;
-            // workouts.forEach((item) =>{
-            //     const workoutName = document.createElement("P");
-            //     workoutName.value = item.name;
-            // });
 
-            let wName = exercise[0].name;
-            let wType = exercise[0].type;
-            let wMuscle = exercise[0].muscle;
+            exercise.forEach((item) =>{
+  
+                let workoutPrinted = `<section class="set ${item.difficulty}">
+                                    <p>Workout: ${item.name}</p>
+                                    <p>Type: ${item.type}</p>
+                                    <p>Difficulty: ${item.difficulty}</p>
+                                    <p>Instructions: ${item.instructions}</p>
+                                    </section>`
+                                    ;
+        
+                let workList = document.querySelector('.work-out-list');
+                workList.innerHTML += workoutPrinted;
 
-            console.log(wName);
-            console.log(wType);
-            console.log(wMuscle);
+                const diffLabel = document.querySelector('#difficultyLabel');
+                diffLabel.classList.remove("hide");
+                
+                const beg = document.querySelector('.beginner');
+                const int = document.querySelector('.intermediate');
+                const exp = document.querySelector('.expert ');
 
+                difficulty1.addEventListener('change', (e) => {
+                    const intensity = e.target.value;
+                    console.log(intensity);
+
+                    if(intensity === "beginner"){
+                        int.classList('.hide');
+                        exp.classList('.hide')
+                    }
+                    else if(intensity === "intermediate"){
+                        beg.classList('.hide');
+                        exp.classList('.hide')
+                    }
+                    else if (intensity === 'expert'){
+                        beg.classList('.hide');
+                        int.classList('.hide')
+                    }
+
+                });
+
+            });
     })
     .catch((err) => console.log("Oops!", err));
+    });
+
+    //Clear Out For new Muscle Selection 
+    const container = document.querySelector('.work-out-list');
+    container.innerHTML = "";
+
 }
 
-
-// var muscle = 'biceps'
-// $.ajax({
-//     method: 'GET',
-//     url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
-//     headers: { 'X-Api-Key': 'IGWz0KSPZ9x79d4rDs6Phw==BPLz2nz7Uzilln0R'},
-//     contentType: 'application/json',
-//     success: function(result) {
-//         console.log(result);
-//     },
-//     error: function ajaxError(jqXHR) {
-//         console.error('Error: ', jqXHR.responseText);
-//     }
-// });
