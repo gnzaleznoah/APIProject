5a8643c80e0c280fdd816a0e5379fef33bfc7eb9
Noah Gonzalez <gnzalezmnoah@gmail.com>
2023-05-17 23:39:08 -0700

    update h2 heading

--- css.js
+++ css.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded', startcss);
 function startcss(){
     let h1 = document.querySelector("h1");
     h1.classList.add('heading1');
+    h1.innerHTML = 'Workout Generator';
+    let h2 = document.createElement("H2");
+    h2.innerText = "API By Nina API";
+    h1.appendChild(h2);
+    h2.classList.add('heading1');
+    h2.classList.add('h2heading');
 }
