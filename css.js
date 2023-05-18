084a3fe1781792072e758fd7ccd978b62a0a3fc3
Noah Gonzalez <gnzalezmnoah@gmail.com>
2023-05-18 08:32:58 -0700

    Upadte Changes and Vid

--- css.js
+++ css.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', startcss);
 
 function startcss(){
+    let headerBox = document.querySelector(".media-box");
+
+    let headerMedia =     
+            `<div class="parent">        
+                <video class="video" src="media/backgroundvid.mp4" autoplay muted></video>
+                    
+                <div class="img-container">
+                <figure>
+                    <img class="background-wall" src="media/noah-workout.jpg" alt="">
+                    <img class="mask-image" src="media/noah-workout2.PNG" alt="">
+                </figure>
+                </div>
+            </div>`;
+        headerBox.innerHTML += headerMedia;
+
 
     let h1 = document.querySelector("h1");
     h1.classList.add('heading1');
