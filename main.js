a2d9424fe7c4f4cc997c5a45e0fae69b779139c4
Troy <tbennett@dvc.edu>
2023-05-20 17:34:07 -0700

    Fixed dropdowns functionality -TB

--- main.js
+++ main.js
@@ -39,7 +39,6 @@ function init(){
     //For Dropdown Menu
      workout1.addEventListener('change',(e) => {
         code = e.target.value;
-
         const url = 'https://api.api-ninjas.com/v1/exercises?muscle=' + code;
         console.log('url', url);
         fetch( url, {
@@ -59,64 +58,75 @@ function init(){
         .then((data) => {
             exercise = data;
             console.log('result1: ', exercise);
-        });
-        //Clear Out For new Muscle Selection 
+
+            //Clear Out For new Muscle Selection 
         const container = document.querySelector('.work-out-list');
         container.innerHTML = "";
 
-        if(exercise.value === exercise.value){
-            exercise.forEach((item) =>{  
-            let workoutPrinted = `<section class="set ${item.difficulty}">
-                                <p>Workout: ${item.name}</p>
-                                <p>Type: ${item.type}</p>
-                                <p>Difficulty: ${item.difficulty}</p>
-                                <p>Instructions: ${item.instructions}</p>
-                                </section>`
-                                ;
-                let workList = document.querySelector('.work-out-list');
-                workList.innerHTML += workoutPrinted;
-               
-            })
+        exercise.forEach((item) =>{  
+        let workoutPrinted = `<section class="set ${item.difficulty}">
+                            <p>Workout: ${item.name}</p>
+                            <p>Type: ${item.type}</p>
+                            <p>Difficulty: ${item.difficulty}</p>
+                            <p>Instructions: ${item.instructions}</p>
+                            </section>`
+                            ;
+            let workList = document.querySelector('.work-out-list');
+            workList.innerHTML += workoutPrinted;
+            
+        })
 
-        }
-        else {       
-            let displayMessage = document.createElement("P");
-            displayMessage.innerHTML = "Select a Muscle to Show Exercises";
-            container.appendChild(displayMessage);
-            console.log('exercise2:', exercise); 
-        }
         console.log('ex3', exercise);
         const diffLabel = document.querySelector('#difficultyLabel');
         diffLabel.classList.remove("hide");
         diffLabel.classList.add("form-scace");
         });
+        
+    });
 
     
     difficulty1.addEventListener('change', (e) => {
-        let container = document.querySelectorAll('.work-out-list section');
-        const beg = document.querySelector('.beginner');
-        const int = document.querySelector('.intermediate');
-        const exp = document.querySelector('.expert');
+        let containers = document.querySelectorAll('.work-out-list section');
+        const notice = document.getElementById('notice');
+        const beg = document.querySelectorAll('.beginner');
+        const int = document.querySelectorAll('.intermediate');
+        const exp = document.querySelectorAll('.expert');
 
         const intensity = e.target.value;
-        // console.log(e.target.value);
 
-        container.forEach((item) => {
+        //hide 'em all
+        containers.forEach((item) => {
             item.classList.add('hide');
-            
-            if(item.classList.contains('beginner')){
-                int.classList.remove('hide');            
-                (exp ? classList.remove('hide')  : '');
-            }
-            else if(item.classList.contains('intermediate')){
-                beg.classList.remove('hide');
-                (exp ? classList.remove('hide')  : '');
-            }
-            else if (item.classList.contains('expert')){
-                beg.classList.remove('hide');
-                int.classList.remove('hide')
-            }
         });
+
+        // show only the ones selected for difficulty
+        // when an if statement only has one line, there is
+        // no need for the curly brackets.
+        switch(intensity) {
+            case 'beginner':
+                if(beg.length === 0) notice.classList.remove('hide');
+                beg.forEach(sec => {
+                    sec.classList.remove('hide')
+                    notice.classList.add('hide');
+                })
+                break;
+            case 'intermediate':
+                if(int.length === 0) notice.classList.remove('hide');
+                int.forEach(sec => {
+                    sec.classList.remove('hide')
+                    notice.classList.add('hide');
+                })
+                break;
+            case 'expert':
+                if(exp.length === 0) notice.classList.remove('hide');
+                exp.forEach(sec => {
+                    sec.classList.remove('hide')
+                    notice.classList.add('hide');
+                })
+                break;
+            default:
+                notice.classList.remove('hide')
+        }
     });
 }
 
